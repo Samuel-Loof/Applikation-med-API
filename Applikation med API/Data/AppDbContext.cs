cb73f3e7fb9b5071c54cc4bab4635c60bd5c88ac
Sam <zaaamuel_96@hotmail.com>
2024-04-02 19:55:33 +0200

    Added example data(SeedData.cs), Products.cs,

--- Applikation med API/Data/AppDbContext.cs
+++ Applikation med API/Data/AppDbContext.cs
@@ -8,6 +8,8 @@ namespace Applikation_med_API.Data
     {
         public DbSet<Account> Accounts { get; set; }
 
+        public DbSet<Product> Products { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
---
+++ Applikation med API/Models/Products.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Applikation_med_API.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string ImageFileName { get; set; } // Namn på bildfilen i undermapp till wwwroot
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+    }
+
+}
+
