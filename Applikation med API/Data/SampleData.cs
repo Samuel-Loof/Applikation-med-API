292f3e613cbb89e3192adc2abd8da904658cc149
gabrielwedin <gabbe_wedin@hotmail.com>
2024-04-10 11:59:50 +0200

    added products and photos

--- Applikation med API/Data/SampleData.cs
+++ Applikation med API/Data/SampleData.cs
@@ -34,255 +34,282 @@ namespace Applikation_med_API.Data
             if (!database.Products.Any())
             {
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Kaffe",
-                        Price = 60,
-                        Category = "Livsmedel",
-                        Description = "Ett fint malet kaffe från Colombia."
-                    });
-                }
 
+                database.Products.Add(new Product
                 {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Te",
-                        Price = 40,
-                        Category = "Livsmedel",
-                        Description = "Grönt te med en fräsch smak av citron."
-                    });
-                }
+                    Name = "Kaffe",
+                    Price = 60,
+                    Category = "Livsmedel",
+                    Description = "Ett fint malet kaffe från Colombia.",
+                    ImagePath = "kaffe.jpg"
+                });
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Laptop",
-                        Price = 10000,
-                        Category = "Elektronik",
-                        Description = "En kraftfull laptop perfekt för både arbete och spel."
-                    });
-                }
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Hörlurar",
-                        Price = 800,
-                        Category = "Elektronik",
-                        Description = "Brusreducerande hörlurar med kristallklart ljud."
-                    });
-                }
 
+                database.Products.Add(new Product
                 {
-                    database.Products.Add(new Product
-                    {
-                        Name = "E-boksläsare",
-                        Price = 1400,
-                        Category = "Elektronik",
-                        Description = "Håll tusentals böcker i handen med vår lättviktiga e-boksläsare."
-                    });
-                }
+                    Name = "Te",
+                    Price = 40,
+                    Category = "Livsmedel",
+                    Description = "Grönt te med en fräsch smak av citron.",
+                    ImagePath = "tea.jpg"
+                });
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Köksknivar",
-                        Price = 900,
-                        Category = "Hem",
-                        Description = "Ett set med professionella köksknivar för varje tillfälle."
-                    });
-                }
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Grytset",
-                        Price = 1500,
-                        Category = "Hem",
-                        Description = "Högkvalitativa grytor och pannor för dina matlagningsbehov."
-                    });
-                }
 
+                database.Products.Add(new Product
                 {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Roman",
-                        Price = 200,
-                        Category = "Böcker",
-                        Description = "En fängslande roman som du inte kan lägga ifrån dig."
-                    });
-                }
+                    Name = "Laptop",
+                    Price = 10000,
+                    Category = "Elektronik",
+                    Description = "En kraftfull laptop perfekt för både arbete och spel.",
+                    ImagePath = "laptop.jpg"
+                });
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Facklitteratur",
-                        Price = 250,
-                        Category = "Böcker",
-                        Description = "Expandera dina kunskaper med vår facklitteratur."
-                    });
-                }
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "T-shirt",
-                        Price = 150,
-                        Category = "Kläder",
-                        Description = "Bekväm och stilren t-shirt i 100% bomull."
-                    });
-                }
 
+                database.Products.Add(new Product
                 {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Jeans",
-                        Price = 500,
-                        Category = "Kläder",
-                        Description = "Slitstarka jeans som passar till allt."
-                    });
-                }
+                    Name = "Hörlurar",
+                    Price = 800,
+                    Category = "Elektronik",
+                    Description = "Brusreducerande hörlurar med kristallklart ljud.",
+                    ImagePath = "headphones.webp"
+                });
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Klocka",
-                        Price = 2600,
-                        Category = "Accessoarer",
-                        Description = "En elegant klocka som kompletterar varje outfit."
-                    });
-                }
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Backpack",
-                        Price = 700,
-                        Category = "Resor",
-                        Description = "Rymlig ryggsäck perfekt för vandring eller stadsliv."
-                    });
-                }
 
+                database.Products.Add(new Product
                 {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Bluetooth Speaker",
-                        Price = 300,
-                        Category = "Elektronik",
-                        Description = "Portabel högtalare med otroligt ljud för storleken."
-                    });
-                }
+                    Name = "iPad",
+                    Price = 4000,
+                    Category = "Elektronik",
+                    Description = "Underhållning vart du än befinner dig med denna utmärkta iPad.",
+                    ImagePath = "ipad.webp"
+                });
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Yogamatta",
-                        Price = 350,
-                        Category = "Sport",
-                        Description = "Högkvalitativ yogamatta för din dagliga yoga."
-                    });
-                }
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Löparskor",
-                        Price = 850,
-                        Category = "Sport",
-                        Description = "Lätta och bekväma löparskor för alla terränger."
-                    });
-                }
 
+                database.Products.Add(new Product
                 {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Sängkläder",
-                        Price = 500,
-                        Category = "Hem",
-                        Description = "Mjuka och behagliga sängkläder för en god natts sömn."
-                    });
-                }
+                    Name = "Köksknivar",
+                    Price = 1000,
+                    Category = "Hem",
+                    Description = "Ett set med professionella köksknivar för varje tillfälle.",
+                    ImagePath = "knivset.webp"
+                });
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Plånbok",
-                        Price = 300,
-                        Category = "Accessoarer",
-                        Description = "Stilren plånbok i äkta läder."
-                    });
-                }
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Handväska",
-                        Price = 1200,
-                        Category = "Accessoarer",
-                        Description = "Elegant handväska i högkvalitativt material."
-                    });
-                }
 
+                database.Products.Add(new Product
                 {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Solglasögon",
-                        Price = 900,
-                        Category = "Accessoarer",
-                        Description = "Moderna solglasögon som skyddar mot UV-strålning."
-                    });
-                }
+                    Name = "Gjutjärnsgryta",
+                    Price = 2000,
+                    Category = "Hem",
+                    Description = "Högkvalitativ gryta som håller i många år framöver.",
+                    ImagePath = "gryta.jpg"
+                });
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Skäggolja",
-                        Price = 250,
-                        Category = "Skönhet",
-                        Description = "Naturlig skäggolja för ett mjukt och välvårdat skägg."
-                    });
-                }
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Ansiktskräm",
-                        Price = 350,
-                        Category = "Skönhet",
-                        Description = "Återfuktande ansiktskräm för alla hudtyper."
-                    });
-                }
 
+                database.Products.Add(new Product
                 {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Parfym",
-                        Price = 700,
-                        Category = "Skönhet",
-                        Description = "Förförisk doft som håller hela dagen."
-                    });
-                }
+                    Name = "A Song Of Ice And Fire",
+                    Price = 500,
+                    Category = "Böcker",
+                    Description = "Prisbelönade fantasy böcker.",
+                    ImagePath = "bok.jpg"
+                });
 
-                {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Kökshandduk",
-                        Price = 100,
-                        Category = "Hem",
-                        Description = "Absorberande kökshandduk i snygg design."
-                    });
-                }
 
+
+                database.Products.Add(new Product
                 {
-                    database.Products.Add(new Product
-                    {
-                        Name = "Vattenflaska",
-                        Price = 200,
-                        Category = "Sport",
-                        Description = "Återanvändbar vattenflaska i rostfritt stål."
-                    });
-                }
+                    Name = "Harry Potter",
+                    Price = 500,
+                    Category = "Böcker",
+                    Description = "Hela Harry Potter serien.",
+                    ImagePath = "bok2.jpg"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "T-shirt",
+                    Price = 150,
+                    Category = "Kläder",
+                    Description = "Bekväm och stilren t-shirt i 100% bomull.",
+                    ImagePath = "t-shirt.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Jeans",
+                    Price = 600,
+                    Category = "Kläder",
+                    Description = "Slitstarka jeans som passar till allt.",
+                    ImagePath = "jeans.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Klocka",
+                    Price = 3000,
+                    Category = "Accessoarer",
+                    Description = "En elegant klocka som kompletterar varje outfit.",
+                    ImagePath = "klocka.png"
+
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Ryggsäck",
+                    Price = 700,
+                    Category = "Resor",
+                    Description = "Rymlig ryggsäck perfekt för vandring eller stadsliv.",
+                    ImagePath = "ryggsäck.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Högtalare",
+                    Price = 1000,
+                    Category = "Elektronik",
+                    Description = "Portabel högtalare med otroligt ljud för storleken.",
+                    ImagePath = "högtalare.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Yogamatta",
+                    Price = 350,
+                    Category = "Sport",
+                    Description = "Högkvalitativ yogamatta för din dagliga yoga.",
+                    ImagePath = "yogamatta.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Löparskor",
+                    Price = 850,
+                    Category = "Sport",
+                    Description = "Lätta och bekväma löparskor för alla terränger.",
+                    ImagePath = "skor.jpg"
+
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Stekpanna",
+                    Price = 700,
+                    Category = "Hem",
+                    Description = "Bäst i test stekpanna.",
+                    ImagePath = "stekpanna.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Plånbok",
+                    Price = 300,
+                    Category = "Accessoarer",
+                    Description = "Stilren plånbok i äkta läder.",
+                    ImagePath = "plånbok.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Fotboll",
+                    Price = 600,
+                    Category = "Sport",
+                    Description = "En mycket hållbar fotboll för både match och träning.",
+                    ImagePath = "fotboll.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Solglasögon",
+                    Price = 900,
+                    Category = "Accessoarer",
+                    Description = "Moderna solglasögon som skyddar mot UV-strålning.",
+                    ImagePath = "solglasögon.jpg"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Skäggolja",
+                    Price = 200,
+                    Category = "Skönhet",
+                    Description = "Naturlig skäggolja för ett mjukt och välvårdat skägg.",
+                    ImagePath = "skäggolja.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Ansiktskräm",
+                    Price = 150,
+                    Category = "Skönhet",
+                    Description = "Återfuktande ansiktskräm för alla hudtyper.",
+                    ImagePath = "ansiktskräm.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Parfym",
+                    Price = 700,
+                    Category = "Skönhet",
+                    Description = "Förförisk doft som håller hela dagen.",
+                    ImagePath = "parfym.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Glas",
+                    Price = 50,
+                    Category = "Hem",
+                    Description = "Glas för valfri dryck.",
+                    ImagePath = "glas.webp"
+                });
+
+
+
+                database.Products.Add(new Product
+                {
+                    Name = "Vattenflaska",
+                    Price = 200,
+                    Category = "Sport",
+                    Description = "Återanvändbar vattenflaska i rostfritt stål.",
+                    ImagePath = "vattenflaska.jpg"
+                });
+
             }
 
             database.SaveChanges();
